62f49d0a33db9ef10805a5bab5a53c0bf1cea39d
sydne <Sydnell822pro@gmail.com>
2025-06-29 19:40:44 +0800

    just changed texts

---
+++ src/Connection.java
@@ -0,0 +1,24 @@
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author sydne
+ */
+class Connection {
+
+    Statement createStatement() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    PreparedStatement prepareStatement(String insert_into_category_name_Values) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
